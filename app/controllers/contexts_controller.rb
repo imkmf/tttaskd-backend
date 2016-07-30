bab70a79a1e42046e0e5ba39e6032c8c3d2ec21f
Kristian Freeman <kristian@simple.com>
2016-07-30 10:28:09 -0700

    Allow creation/updating of projects and contexts

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -14,4 +14,34 @@ class ContextsController < ApplicationController
       render json: { context: @context }
     end
   end
+
+  def create
+    cp = context_params.to_h
+    cp[:user_id] = @current_user.id
+
+    @context = Context.new(cp)
+    if @context.valid?
+      @context.save!
+      render json: { context: @context }
+    else
+      render json: { errors: @context.errors }
+    end
+  end
+
+  def update
+    @context = Context.find(params[:id])
+    if @context.user != @current_user
+      render json: { errors: { context: ['Unauthorized'] }}, status: :unauthorized
+    else
+      @context.update_attributes!(context_params)
+      render json: { context: @context }
+    end
+  end
+
+  private
+  def context_params
+    params.require(:context).permit(
+      :name,
+    )
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,4 +14,34 @@ class ProjectsController < ApplicationController
       render json: { project: @project }
     end
   end
+
+  def create
+    cp = project_params.to_h
+    cp[:user_id] = @current_user.id
+
+    @project = Project.new(cp)
+    if @project.valid?
+      @project.save!
+      render json: { project: @project }
+    else
+      render json: { errors: @project.errors }
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.user != @current_user
+      render json: { errors: { project: ['Unauthorized'] }}, status: :unauthorized
+    else
+      @project.update_attributes!(project_params)
+      render json: { project: @project }
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :name,
+    )
+  end
 end
