bf134c096d41e691f4fb059b05c760e0bc73e725
Kristian Freeman <kristian@simple.com>
2016-09-02 22:23:56 -0400

    Whoops

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,4 @@ class PagesController < ApplicationController
   def root
     render text: 'ok'
   end
-
-  def letsencrypt
-    render text: "m7MObgxRL-JctHHKqYIpD_Vm1_AcTtrb_XCsuZkgacE.18axvS74ZtxI4xiWpoZQHuDhazxSf0xBjkHDWTpaTSo"
-  end
 end
